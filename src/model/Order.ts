68e52a830f76dd3355831f13891b57e4b590bf35
Elhadji <elhadji@ctsfares.com>
2020-03-06 15:20:12 -0500

    update customer and order models

--- src/model/Order.ts
+++ src/model/Order.ts
@@ -1,10 +1,12 @@
 import {Customer} from "./Customer";
+import {Product} from "./Product";
+import {User} from "./User";
 
 export class Order {
     readonly reference!: string;
     customer!: Customer;
-    user!: string;
+    user!: User;
     orderedAt!: string;
-    products!: string;
+    products!: Product[];
     deliveredAt!: string;
 }
