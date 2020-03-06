ccbb8aa3ca812194b16e62dc397eef40e5c6b892
Cheikh Ahmadou Bamba DIAGNE <ahmadoubamba.diagne@finditech.com>
2020-03-06 21:14:54 +0000

    fixe user model

--- src/model/User.ts
+++ src/model/User.ts
@@ -1,13 +1,15 @@
 import * as mongoose from "mongoose";
-import {Schema} from "mongoose";
 
-let User = mongoose.model("User", new Schema<any>({
+export const UserSchema = new mongoose.Schema<any>({
     ref: String,
     firstName: String,
     lastName: String,
     phone: Number,
     adress: String,
     email: String
-}));
+});
+
+let User = mongoose.model("User", UserSchema);
+
 
 export default User
